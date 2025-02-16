1ff0175a8e2fd23119fe1dbd3187e09441e11a28
jose mundo <joseemundop@gmail.com>
2025-02-16 14:08:18 -0400

    bp changed

--- src/hooks/breakpointHandler.ts
+++ src/hooks/breakpointHandler.ts
@@ -1,11 +1,11 @@
 import { useMediaQuery } from '@mantine/hooks'
 
 export const useBreakPointHandler = () => {
-  const isXS = useMediaQuery('(max-width: 576px)');
-  const isSM = useMediaQuery('(min-width: 576px) and (max-width: 768px)');
-  const isMD = useMediaQuery('(min-width: 768px) and (max-width: 992px)');
-  const isLG = useMediaQuery('(min-width: 992px) and (max-width: 1200px)');
-  const isXL = useMediaQuery('(min-width: 1200px)');
+  const isXS = useMediaQuery('(max-width: 480px)');
+  const isSM = useMediaQuery('(min-width: 480px) and (max-width: 768px)');
+  const isMD = useMediaQuery('(min-width: 768px) and (max-width: 1024px)');
+  const isLG = useMediaQuery('(min-width: 1024px) and (max-width: 1280px)');
+  const isXL = useMediaQuery('(min-width: 1280px)');
 
   const getByBreakPoint = <T>(xs: T, sm: T, md: T, lg: T, xl: T): T => {
     if (isXS) return xs
