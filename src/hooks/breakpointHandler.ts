27df1b2f73a254f0f759887f2499b0a5f86b43fc
jose mundo <joseemundop@gmail.com>
2025-02-16 14:11:36 -0400

    bp changed2

--- src/hooks/breakpointHandler.ts
+++ src/hooks/breakpointHandler.ts
@@ -4,8 +4,8 @@ export const useBreakPointHandler = () => {
   const isXS = useMediaQuery('(max-width: 480px)');
   const isSM = useMediaQuery('(min-width: 480px) and (max-width: 768px)');
   const isMD = useMediaQuery('(min-width: 768px) and (max-width: 1024px)');
-  const isLG = useMediaQuery('(min-width: 1024px) and (max-width: 1280px)');
-  const isXL = useMediaQuery('(min-width: 1280px)');
+  const isLG = useMediaQuery('(min-width: 1024px) and (max-width: 1440px)');
+  const isXL = useMediaQuery('(min-width: 1440px)');
 
   const getByBreakPoint = <T>(xs: T, sm: T, md: T, lg: T, xl: T): T => {
     if (isXS) return xs
