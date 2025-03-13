220c44f452a3e56c2c9c31afd315c590d4112c85
jose mundo <joseemundop@gmail.com>
2025-03-13 14:51:06 -0400

    pre-final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export const ThemeMantine = createTheme({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" {...mantineHtmlProps} style={{ fontSize: '10px' }}>
+    <html lang="en" {...mantineHtmlProps} style={{ fontSize: '16px' }}>
       <head>
         <ColorSchemeScript />
       </head>
