6faac6152c9027c0845584c0a6d9154c39e0721a
jose mundo <joseemundop@gmail.com>
2025-05-01 14:19:01 -0400

    test role5

--- src/server/utilities/cookieManager.ts
+++ src/server/utilities/cookieManager.ts
@@ -2,8 +2,7 @@ import cookie from 'cookie'
 import { cookieManagerI, createCookieProps } from './interfaces'
 
 const createCookie = ({ data, maxAge = 60 * 60 }: createCookieProps): string => {
-  if (!cookie) throw Error('cookie es undefined')
-  const _cookie = cookie.serialize('token', data, { secure: process.env.NODE_ENV !== 'development', httpOnly: true, maxAge })
+  const _cookie = cookie.serialize('token', data, { secure: process.env.NODE_ENV !== 'development', httpOnly: true, path:'/', maxAge })
   return _cookie
 }
 
