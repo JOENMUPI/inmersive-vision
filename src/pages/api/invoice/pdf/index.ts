d98500ac0061c9228e67236ce14ab322b776342c
jose mundo <joseemundop@gmail.com>
2025-06-15 10:45:51 +0200

    pdf

--- src/pages/api/invoice/pdf/index.ts
+++
@@ -1,9 +0,0 @@
-import { errorMethod, createPdf } from '@/server/modules/invoice/infraestructure/pdf.controller'
-import { adapterResponseI } from '@/server/utilities/interfaces'
-import { methodHTTP } from '@/server/utilities/enums'
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
-export default function handler(req: NextApiRequest, res: NextApiResponse<adapterResponseI>) {
-  if (req.method === methodHTTP.POST) createPdf(req, res)  
-  else errorMethod(req, res)
-}
